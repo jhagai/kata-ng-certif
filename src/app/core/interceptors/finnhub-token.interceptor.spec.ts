7a7143b8ff627be6f1edd9ac9b246e972638aa78
joelhagai <joel.hagai@gmail.com>
2022-10-10 00:07:14 +0200

    Fix app + interceptor unit test.

--- src/app/core/interceptors/finnhub-token.interceptor.spec.ts
+++ src/app/core/interceptors/finnhub-token.interceptor.spec.ts
@@ -1,12 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { FinnhubInterceptor } from './finnhub-interceptor.service';
+import {FinnhubInterceptor} from './finnhub-interceptor.service';
+import {FINNHUB_BASE_URL} from "../../shared/providers/finnhub-base-url.injection-token";
+import {FINN_HUB_TOKEN} from "../../shared/providers/finnhub-token.injection-token";
 
 describe('FinnhubInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
-      FinnhubInterceptor
-      ]
+      FinnhubInterceptor,
+      {provide: FINNHUB_BASE_URL, useValue: 'baseURL'},
+      {provide: FINN_HUB_TOKEN, useValue: 'token'}
+    ]
   }));
 
   it('should be created', () => {
